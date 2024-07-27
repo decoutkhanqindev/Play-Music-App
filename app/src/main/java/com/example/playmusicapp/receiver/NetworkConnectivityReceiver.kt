f243c4e3d13c54b4366620cb4b8f0ab34b1f53ab
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-27 22:59:34 +0700

    create network broadcast receiver

--- app/src/main/java/com/example/playmusicapp/receiver/NetworkConnectivityReceiver.kt
+++ app/src/main/java/com/example/playmusicapp/receiver/NetworkConnectivityReceiver.kt
@@ -3,9 +3,31 @@ package com.example.playmusicapp.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+import android.widget.Toast
 
-class NetworkConnectivityReceiver: BroadcastReceiver() {
+class NetworkConnectivityReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        if (intent?.action == Intent.ACTION_SCREEN_ON) {
+            // quan ly ket noi mang
+            val connectivityManager =
+                context?.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+            // lay thong tin ve kha nang cua mang hien tai dang hoat dong
+            val networkCapabilities =
+                connectivityManager?.getNetworkCapabilities(connectivityManager.activeNetwork)
+            // kiem tra ket noi mang
+            val isConnected =
+                networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+
+            if (isConnected) {
+                Toast.makeText(context, "Network connected", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Network disconnected", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Log.d("NetworkConnectivityReceiver", "Received unexpected action: ${intent?.action}")
+        }
     }
 }
